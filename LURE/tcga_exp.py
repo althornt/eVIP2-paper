dd748d4db20d3d1967951428cd4a7347fabc8f79
Alexis Thornton <althornt@ucsc.edu>
2021-01-30 19:53:27 -0500

    add more sample labels

--- LURE/tcga_exp.py
+++ LURE/tcga_exp.py
@@ -8,6 +8,7 @@ import matplotlib
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import json
 
 def main():
     #from LURE_lolli_
@@ -15,8 +16,8 @@ def main():
 
     #Xena - gene-leveltranscription estimates, as in log2(x+1) transformed RSEM normalized count
     # tcga_exp = pd.read_csv("inputs/HiSeqV2", sep="\t",index_col=0)
-    tcga_exp = pd.read_csv("inputs/XenaCOADREAD_HiSeqV2", sep="\t",index_col=0)
-    tcga_exp.columns =  [i[:-3] for i in tcga_exp.columns]
+    tcga_exp_coadread = pd.read_csv("inputs/XenaCOADREAD_HiSeqV2", sep="\t",index_col=0)
+    tcga_exp_coadread.columns =  [i[:-3] for i in tcga_exp_coadread.columns]
 
     #add  kras status to mut df
     kras_mut =  pd.read_csv("inputs/cbioportal_KRAS_TCGA_COAD_mutations.txt", sep="\t",index_col  = "SAMPLE_ID")
@@ -25,33 +26,38 @@ def main():
     score_mut = score_mut.merge(kras_mut["KRAS"], how='outer', left_index=True, right_index=True)
     score_mut = score_mut[score_mut['x'].notna()]
 
+    ##########################
+    # get gene lists
+    #########################
+
+    # #import kegg gene set
+    # kegg_mapk_genes = None
+    # wnt_genes = None
+    # cancer_genes = None
+    # cytokine_genes = None
+    # all_kegg_genes = []
+    # with open("inputs/c2.cp.kegg.v7.2.symbols.gmt", "r") as kegg:
+    #     for line in kegg:
+    #         all_kegg_genes.extend(line.split()[2:])
+    #         if line.split()[0] =="KEGG_MAPK_SIGNALING_PATHWAY":
+    #             kegg_mapk_genes = line.split()[2:]
+    #         if line.split()[0] =="KEGG_WNT_SIGNALING_PATHWAY":
+    #             wnt_genes = line.split()[2:]
+    #         if line.split()[0] =="KEGG_PATHWAYS_IN_CANCER":
+    #             cancer_genes = line.split()[2:]
+    #         if line.split()[0] =="KEGG_CYTOKINE_CYTOKINE_RECEPTOR_INTERACTION":
+    #             cytokine_genes = line.split()[2:]
 
 
-    #import kegg gene set
-    kegg_mapk_genes = None
-    wnt_genes = None
-    cancer_genes = None
-    cytokine_genes = None
-    with open("inputs/c2.cp.kegg.v7.2.symbols.gmt", "r") as kegg:
-        for line in kegg:
-            if line.split()[0] =="KEGG_MAPK_SIGNALING_PATHWAY":
-                kegg_mapk_genes = line.split()[2:]
-            if line.split()[0] =="KEGG_WNT_SIGNALING_PATHWAY":
-                wnt_genes = line.split()[2:]
-            if line.split()[0] =="KEGG_PATHWAYS_IN_CANCER":
-                cancer_genes = line.split()[2:]
-            if line.split()[0] =="KEGG_CYTOKINE_CYTOKINE_RECEPTOR_INTERACTION":
-                cytokine_genes = line.split()[2:]
+    # nfkb_genes = []
+    # with open("inputs/h.all.v6.0.symbols.gmt", "r") as hm:
+    #     for line in hm:
+    #         # if line.split()[0] in ["HALLMARK_KRAS_SIGNALING_UP","HALLMARK_KRAS_SIGNALING_DN","HALLMARK_TNFA_SIGNALING_VIA_NFKB","HALLMARK_HYPOXIA","HALLMARK_EPITHELIAL_MESENCHYMAL_TRANSITION"]:
+    #         if line.split()[0] in ["HALLMARK_TNFA_SIGNALING_VIA_NFKB"]:
+    #
+    #         # if line.split()[0] in ["HALLMARK_KRAS_SIGNALING_UP","HALLMARK_KRAS_SIGNALING_DN"]:
+    #             nfkb_genes.extend(line.split()[2:])
 
-    """
-    genes = []
-    with open("inputs/h.all.v6.0.symbols.gmt", "r") as hm:
-        for line in hm:
-            # if line.split()[0] in ["HALLMARK_KRAS_SIGNALING_UP","HALLMARK_KRAS_SIGNALING_DN","HALLMARK_TNFA_SIGNALING_VIA_NFKB","HALLMARK_HYPOXIA","HALLMARK_EPITHELIAL_MESENCHYMAL_TRANSITION"]:
-
-            if line.split()[0] in ["HALLMARK_KRAS_SIGNALING_UP","HALLMARK_KRAS_SIGNALING_DN"]:
-                genes.extend(line.split()[2:])
-    """
 
     #https://www.nature.com/articles/s41698-018-0051-4 "MAPK Pathway Activity Score "
     global mapkgenes
@@ -78,29 +84,166 @@ def main():
     hypoxia = "ORAI3 FBP1 VEGFA MVD GAS6 CYB5R3 ZBTB44 MDM2 PRELID2 CCNG1 FAM117B CASP6 TRAF3 PRP1B".split()
     """
 
-    clustermap(score_mut,tcga_exp,kegg_mapk_genes+mapkgenes+wnt_genes+mmr,"kegg_mapk_genes_andtengene" )
+    # clustermap(score_mut,tcga_exp_coadread,kegg_mapk_genes+mapkgenes+wnt_genes+mmr,"kegg_mapk_genes_andtengene" )
 
     lobodo_ras = open("inputs/lobodo_RAS_signature.txt","r").read().splitlines()
-    clustermap(score_mut,tcga_exp,lobodo_ras,"lobodo_ras" )
+    # clustermap(score_mut,tcga_exp_coadread,lobodo_ras,"lobodo_ras" )
 
     popovici = pd.read_csv( "inputs/popovici_braf_signature.tsv",sep="\t").drop(columns=["Pair","Pair.1"])
     popovici_genes = [item for sublist in popovici.values.tolist() for item in sublist]
 
-    # clustermap(score_mut,tcga_exp,mapkgenes,"mapk" )
-    # clustermap(score_mut,tcga_exp,popovici_genes+mapkgenes,"braf_sig_mapk" )
-    # clustermap(score_mut,tcga_exp,mapkgenes+wnt+mmr,"og" )
+    # clustermap(score_mut,tcga_exp_coadread,mapkgenes,"mapk" )
+    # clustermap(score_mut,tcga_exp_coadread,popovici_genes+mapkgenes,"braf_sig_mapk" )
+    # clustermap(score_mut,tcga_exp_coadread,mapkgenes+wnt+mmr,"og" )
+
+
+    ##########################
+    # make clustermaps
+    #########################
+
+
+    with open("inputs/hallmark.all.v6.0_c2.cp.kegg.v6.0_c2.cp.reactome.v6.0_symbols.json") as f:
+        pathway_dict = json.load(f)
+    all_pathway_genes =  [item for sublist in  pathway_dict.values() for item in sublist]
+
+    """
+    dusp4_like_df = tcga_exp_coadread.loc[set(all_pathway_genes)].transpose().corr()["DUSP4"].sort_values(ascending=False)
+    dusp4_like_head = dusp4_like_df.head(30).index.tolist()
+    dusp4_like_tail = dusp4_like_df.dropna().tail(30).index.tolist()
+    print(dusp4_like_head)
+    print(dusp4_like_tail)
+
+    etv5_like_df = tcga_exp_coadread.loc[set(all_pathway_genes)].transpose().corr()["ETV5"].sort_values(ascending=False)
+    etv5_like_head = etv5_like_df.head(30).index.tolist()
+    etv5_like_tail = etv5_like_df.dropna().tail(30).index.tolist()
+    print(etv5_like_head)
+    print(etv5_like_tail)
+    """
+
+    pathways_eVIP =["HALLMARK_KRAS_SIGNALING_UP","HALLMARK_KRAS_SIGNALING_DN","HALLMARK_TNFA_SIGNALING_VIA_NFKB",
+            "HALLMARK_HYPOXIA","HALLMARK_EPITHELIAL_MESENCHYMAL_TRANSITION","KEGG_MAPK_SIGNALING_PATHWAY",
+            "REACTOME_MAPK_TARGETS_NUCLEAR_EVENTS_MEDIATED_BY_MAP_KINASES","REACTOME_ERK_MAPK_TARGETS"]
+
+    eVIP_pway_genes = []
+    for i in pathways_eVIP:
+        eVIP_pway_genes.extend(pathway_dict[i])
+
+    # for i in pathways_eVIP:
+    #     for j in pathway_dict[i]:
+    #         if j in dusp4_like:
+    #             print(i,j)
+    #         if j in etv5_like:
+    #             print(i,j)
+
+    # pathway_matches = {}
+    #
+    # for i in [dusp4_like+etv5_like]:
+    #     for k,v in pathway_dict.items():
+    #         if i in v:
+    #             print(i,k)
+    #             try:
+    #                 pathway_matches[k].append(i)
+    #             except:
+    #                 pathway_matches = [i]
+
+    # from collections import defaultdict
+    #
+    # pathway_matches_dict = defaultdict(list)
+    # for i in dusp4_like+etv5_like:
+    #     for k,v in pathway_dict.items():
+    #         if i in v:
+    #             print(i)
+    #             pathway_matches_dict[k].append(i)
+    #
+    #
+    # print(pathway_matches_dict)
+    # for k in sorted(pathway_matches_dict, key=lambda k: len(pathway_matches_dict[k]), reverse=True):
+    #     if k.startswith("RE"):
+    #         print(k,v)
+    #
+
+    """
+    ############################################################
+    # Finding high corr genes in RNF43 trunc samples
+    ############################################################
+
+    #find tcga samples in both df
+    samples = [i for i in tcga_exp_coadread.columns if i in score_mut.index ]
+    tcga_exp = tcga_exp_coadread[samples]
+    score_mut_rnf43 = score_mut[score_mut.index.isin(samples)]["RNF43_TRUNCATING"].to_frame()
+    tcga_exp = tcga_exp.append(score_mut_rnf43["RNF43_TRUNCATING"].transpose())
+
+    #get samples with RNF43 trunc
+    tcga_exp_rnf43 = tcga_exp.loc[:,tcga_exp.loc["RNF43_TRUNCATING"]=="TRUNCATING"].drop(["RNF43_TRUNCATING"], axis=0)
+
+    #reduce to eVIP pway genes
+    tcga_exp_rnf43 = tcga_exp_rnf43[tcga_exp_rnf43.index.isin(eVIP_pway_genes)]
+
+    #find correlated genes
+    corr = tcga_exp_rnf43.astype(float).transpose().corr().abs()
+    corr = corr.unstack()
+    corr = corr.sort_values(kind="quicksort",ascending=False)
+    corr_clean = corr.dropna()
+    corr_clean = corr_clean[corr_clean>0.95]
+    corr_clean = corr_clean[corr_clean<0.999999999] #remove self-correlations
+    candidate_genes = corr_clean.index.tolist()
+    candidate_genes = list(set([item for sublist in candidate_genes for item in sublist]))
+    print(len(candidate_genes))
+    """
+    """
+    hypoxia tried - VHL     HIF1AN
+
+    EMT - CDH1 is down in half but makes clustering worse
+        - VIM is up in  half but makes clustering worse (same half as CDH1)
+        FN1
+        TJP1
+        OCLN
+        CDH2
+        DKK1
+    """
+
+
+
+    global hypoxia_genes
+    # hypoxia_genes = ["HIF1A","ENO2"]
+    hypoxia_genes = ["HIF1A"]
+
+    global emt_genes
+    # emt_genes = ["TWIST1","SNAI1","COMP","CDH1","VIM","DKK1","CDH2","FN1"]
+    emt_genes = ["TWIST1","SNAI1"]
+
+    global nfkb_genes
+    # nfkb_genes = ["NFKB1","NFKB2","RELB","IL8","IL6"]
+    nfkb_genes = ["NFKB1","NFKB2","RELB"]
+
+
+    for df in [tcga_exp_coadread]:
+        # clustermap(score_mut,df,mapkgenes+wnt+mmr,"og" )
+        # clustermap(score_mut,df,mapkgenes+wnt+mmr+["TWIST1","SNAI1"]+["HIF1A","ARNT"]+["MACC1"],"og" )
+        clustermap(score_mut,df,mapkgenes+wnt+mmr+hypoxia_genes+nfkb_genes+["TWIST1","SNAI1"],"og" )
+
+        # clustermap(score_mut,df,mapkgenes+wnt+mmr+["TWIST1","SNAI1","COMP","CDH1","VIM","DKK1","CDH2","FN1"],"TWIST1" )
+        # clustermap(score_mut,df,mapkgenes+wnt+mmr+nfkb_genes,"nfkb_genes" )
+        # clustermap(score_mut,df,mapkgenes+wnt+mmr+["TWIST1","SNAI1","COMP","CDH1","VIM","DKK1","CDH2","FN1"]+["HIF1A","ENO2"],"og_with_emt_hypoxia" )
+        # clustermap(score_mut,df,mapkgenes+wnt+mmr+emt_genes+hypoxia_genes+NFKB_genes+["MAPK14","MAPK9"],"og_with_emt_hypoxia_tnf" )
+        # clustermap(score_mut,df,mapkgenes+wnt+mmr+emt_genes+hypoxia_genes+NFKB_genes,"og_with_emt_hypoxia_tnf" )
+        # clustermap(score_mut,df,NFKB_genes,"NFKB_genes" )
+        # clustermap(score_mut,df,mmr+wnt+dusp4_like_tail[0:5]+dusp4_like_head[0:5]+etv5_like_tail[0:5]+etv5_like_head[0:5],"og_new" )
+        # clustermap(score_mut,df,mapkgenes+wnt+mmr+candidate_genes,"high_corr" )
+
+
+
+
 
 
 def clustermap(score_mut,tcga_exp, gene_list,  label ):
     tcga_exp = tcga_exp[tcga_exp.index.isin(gene_list)]
-
 
     #remove low std genes
     # tcga_exp = tcga_exp.loc[tcga_exp.std(axis=1) > 2, :]
 
     #drop duplicated samples
     tcga_exp = tcga_exp.loc[:,~tcga_exp.columns.duplicated(keep="first")]
-
 
     #find tcga samples in both
     samples = [i for i in tcga_exp.columns if i in score_mut.index ]
@@ -109,6 +252,13 @@ def clustermap(score_mut,tcga_exp, gene_list,  label ):
     #rename RNF43 so its not confused with gene expression row
     score_mut = score_mut.rename(columns={"RNF43": "RNF43_muts","KRAS":"KRAS_muts"})
     tcga_exp = tcga_exp.append(score_mut[["RNF43_TRUNCATING","BRAF_MISSENSE","x","RNF43_muts","KRAS_muts"]].transpose())
+
+    xenaclinical = pd.read_csv("inputs/COADREAD_clinicalMatrix", sep="\t",
+                            index_col = "_PATIENT")
+    xenaclinical = xenaclinical[xenaclinical.index.isin(tcga_exp.columns)]
+    xenaclinical = xenaclinical[xenaclinical.index.isin(tcga_exp)]
+    xenaclinical = xenaclinical.loc[~xenaclinical.index.duplicated(keep="first")]
+    tcga_exp = tcga_exp.append(xenaclinical["CDE_ID_3226963"].transpose())
 
     ##################################
     # adding color columns
@@ -119,9 +269,19 @@ def clustermap(score_mut,tcga_exp, gene_list,  label ):
     lure_score = tcga_exp.loc["x"].tolist()
     RNF43_muts  = tcga_exp.loc["RNF43_muts"].tolist()
     KRAS_muts = tcga_exp.loc["KRAS_muts"].tolist()
+    msi_status = tcga_exp.loc["CDE_ID_3226963"].tolist()
 
-
-# "G12D","G12S", "G12R", "G12C", "G12V", "G12A"
+    #convert MSI status to color
+    msi_status_c = []
+    for i in msi_status:
+        if i == "MSI-H":
+            msi_status_c.append("darkred")
+        elif i == "MSI-L":
+            msi_status_c.append("indianred")
+        elif i == "MSS":
+            msi_status_c.append("silver")
+        else:
+            msi_status_c.append("#f0edf2")
 
     #convert RNF43 mut status to color
     kras_status_c = []
@@ -132,9 +292,6 @@ def clustermap(score_mut,tcga_exp, gene_list,  label ):
             kras_status_c.append("black")
         else:
             kras_status_c.append("#f0edf2")
-
-
-
 
     #convert RNF43 mut status to color
     rnf43_status_c = []
@@ -165,7 +322,6 @@ def clustermap(score_mut,tcga_exp, gene_list,  label ):
     tcga_exp.loc['x_color'] = tcga_exp.loc['x'].apply(lambda x: mcolors.to_hex(mapper.to_rgba(x)))
     lure_scale_c = tcga_exp.loc['x_color'].tolist()
 
-
     #left vs right side
     clinical = pd.read_csv("inputs/coadread_tcga_clinical_data.tsv", sep="\t",
                             index_col = "Patient ID")
@@ -187,16 +343,6 @@ def clustermap(score_mut,tcga_exp, gene_list,  label ):
             side_status_c.append("#f0edf2")
 
 
-    """
-    #clinical from Xena that has MSI status
-    xenaclinical = pd.read_csv("inputs/COADREAD_clinicalMatrix", sep="\t",
-                            index_col = "_PATIENT")
-    xenaclinical = xenaclinical[xenaclinical.index.isin(tcga_exp.columns)]["MSI_updated_Oct62011"]
-
-    print(xenaclinical.value_counts())
-    """
-
-
     #prep df for clustermap
     tcga_exp = tcga_exp.drop("RNF43_TRUNCATING", axis=0)
     tcga_exp = tcga_exp.drop("BRAF_MISSENSE", axis=0)
@@ -204,6 +350,7 @@ def clustermap(score_mut,tcga_exp, gene_list,  label ):
     tcga_exp = tcga_exp.drop("x_color", axis=0)
     tcga_exp = tcga_exp.drop("RNF43_muts", axis=0)
     tcga_exp = tcga_exp.drop("KRAS_muts", axis=0)
+    tcga_exp = tcga_exp.drop("CDE_ID_3226963", axis=0)
 
     tcga_exp = tcga_exp.convert_objects(convert_numeric=True)
 
@@ -212,12 +359,17 @@ def clustermap(score_mut,tcga_exp, gene_list,  label ):
     gene_colors = []
     for i in tcga_exp.index.tolist():
         if i in mapkgenes:
-            gene_colors.append("slateblue")
+            gene_colors.append("black")
         elif i in wnt:
-            gene_colors.append("slategrey")
+            gene_colors.append("cornflowerblue")
         elif i in mmr:
             gene_colors.append("plum")
-
+        elif i in nfkb_genes:
+            gene_colors.append("purple")
+        elif i in hypoxia_genes:
+            gene_colors.append("crimson")
+        elif i in emt_genes:
+            gene_colors.append("slateblue")
         else:
             gene_colors.append("pink")
 
@@ -230,15 +382,16 @@ def clustermap(score_mut,tcga_exp, gene_list,  label ):
     #             col_colors=[lure_scale_c,braf_status_c,rnf43_status_c,RNF43_mut_type_c],
     #             row_cluster=True, cbar_pos=None,vmin=-5, vmax=4)
 
+
     sns.clustermap(tcga_exp,z_score=0,center=0,cmap="bwr",xticklabels=False,
                 yticklabels=True,row_colors=gene_colors, method = "ward",
-                colors_ratio = .02, figsize = (12,8),
-                col_colors=[lure_scale_c,braf_status_c,rnf43_status_c,RNF43_mut_type_c,kras_status_c],
+                colors_ratio = .03, figsize = (12,8),
+                col_colors=[lure_scale_c,braf_status_c,rnf43_status_c,RNF43_mut_type_c,kras_status_c,msi_status_c],
                 row_cluster=True)
 
     plt.tight_layout()
-    plt.savefig("outputs/tcga_heatmap_"+label+".png",bbox_inches = "tight",dpi=400)
-    plt.savefig("outputs/tcga_heatmap_"+label+".svg",bbox_inches = "tight",dpi=400)
+    plt.savefig("outputs/clustermaps/tcga_heatmap_"+label+".png",bbox_inches = "tight",dpi=400)
+    plt.savefig("outputs/clustermaps/tcga_heatmap_"+label+".svg",bbox_inches = "tight",dpi=400)
 
     plt.clf()
     plt.close()
@@ -247,10 +400,14 @@ def clustermap(score_mut,tcga_exp, gene_list,  label ):
     # Putting legends and colorbars in another file
     ##################################################################
 
-    mapk_patch = mpatches.Patch(color='slateblue', label='MAPK genes')
-    wnt_patch = mpatches.Patch(color='slategrey', label='WNT genes')
-    mmr_patch = mpatches.Patch(color='plum', label='MMR gene')
-    plt.legend(handles=[wnt_patch, mapk_patch, mmr_patch],loc='upper center')
+    mapk_patch = mpatches.Patch(color='black', label='MAPK')
+    nfkb_patch = mpatches.Patch(color='purple', label='NFKB')
+    hypoxia_patch = mpatches.Patch(color='crimson', label='Hypoxia')
+    emt_patch = mpatches.Patch(color='slateblue', label='EMT')
+    wnt_patch = mpatches.Patch(color='cornflowerblue', label='WNT')
+    mmr_patch = mpatches.Patch(color='plum', label='MMR')
+    plt.legend(handles=[mapk_patch,nfkb_patch,hypoxia_patch,wnt_patch,emt_patch, mmr_patch],loc='upper center')
+
 
     #color baar for LURE classifier score
     sm = plt.cm.ScalarMappable(cmap=plt.cm.YlGn, norm=plt.Normalize(vmin=0, vmax=1))
@@ -262,6 +419,7 @@ def clustermap(score_mut,tcga_exp, gene_list,  label ):
     sm._A = []
     plt.colorbar(sm)
 
+    plt.savefig("outputs/tcga_heatmap_legend.png",bbox_inches = "tight",dpi=400)
     plt.savefig("outputs/tcga_heatmap_legend.svg",bbox_inches = "tight",dpi=400)
     plt.clf()
     plt.close()
